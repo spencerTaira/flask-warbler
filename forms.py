a3211b5e13d3383192b284e56cc6af0c011e0cdb
Spencer Taira <spencermtaira@gmail.com>
2022-10-26 11:42:53 -0700

    added location and bio to user profile in html pages

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, InputRequired
 
 
 class MessageForm(FlaskForm):
